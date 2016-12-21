f1dc9a59f02ef5428104cc6946a7163e7314beca
Stephen Nayfach <snayfach@shattuck.ucsf.edu>
2016-12-20 16:12:07 -0800

    Updated snp_diversity.py
    -uses parse.py
    -compatible with new output from merge_midas.py

--- midas/parse.py
+++ midas/parse.py
@@ -36,12 +36,12 @@ class Species:
 
 	def init_paths(self):
 		self.paths = {}
-		for type in ['alt_allele', 'depth', 'info', 'ref_freq', 'summary']:
+		for type in ['freq', 'depth', 'info', 'summary']:
 			self.paths[type] = '%s/snps_%s.txt' % (self.dir, type)
 
 	def init_files(self):
 		self.files = {}
-		for type in ['alt_allele', 'depth', 'info', 'ref_freq', 'summary']:
+		for type in ['freq', 'depth', 'info', 'summary']:
 			file = open(self.paths[type])
 			if type in ['info', 'summary']:
 				self.files[type] = csv.DictReader(file, delimiter='\t')
@@ -50,7 +50,7 @@ class Species:
 
 	def init_samples(self):
 		self.sample_ids = None
-		for file in ['alt_allele', 'depth', 'ref_freq']:
+		for file in ['freq', 'depth']:
 			self.sample_ids = next(self.files[file])[1:]
 
 
@@ -58,38 +58,36 @@ class GenomicSite:
 	""" Base class for genomic sites """
 	def __init__(self, species, samples):
 		try:
+			# fetch site info
+			info = next(species.files['info'])
+			self.id = info['site_id']
+			self.ref_allele = info['ref_allele']
+			self.minor_allele = info['minor_allele']
+			self.major_allele = info['major_allele']
+			self.gene_id = info['gene_id']
+			self.site_type = info['site_type']
+						
+			# copy samples
 			self.samples = samples
+			
+			# fetch site data from freq and depth matrixes
 			self.fetch_row(species)
-			self.info = next(species.files['info'])
-			self.id=1
-			self.site_id()
+
 		except StopIteration:
 			self.id = None
-
-	def site_id(self):
-		""" parse info from site_id """
-		self.id = self.info['site_id']
-		self.ref_id, self.ref_pos, self.ref_allele = self.id.rsplit('|', 2)
 		
 	def fetch_row(self, species):
-		""" fetch next row from matrices """
-		self.sample_ids = species.sample_ids
-		values = zip(self.sample_ids,
-		             next(species.files['ref_freq'])[1:],
-					 next(species.files['depth'])[1:],
-					 next(species.files['alt_allele'])[1:]
-					 )
-		for sample_id, ref_freq, depth, alt_allele in values:
-			try:
-				sample = self.samples[sample_id]
-				sample.ref_freq = float(ref_freq) if ref_freq != 'NA' else 0.0
-				sample.depth = int(depth)
-				sample.alt_allele = alt_allele
-			except KeyError:
-				pass
+		""" Fetch next row from freq and depth matrices
+		    Store in sample objects: sample.freq, sample.depth """
+		freqs = next(species.files['freq'])[1:]
+		depths = next(species.files['depth'])[1:]
+		for sample in self.samples.values():
+			self.samples[sample.id].freq = float(freqs[sample.index])
+			self.samples[sample.id].depth = int(depths[sample.index])
 				
-	def filter_samples(self, site_depth, site_ratio):
-		""" filter samples at site based on coverage """
+	def flag_samples(self, site_depth, site_ratio):
+		""" Filter samples at site based on coverage
+			Set flag: sample.keep = [True/False] """
 		for sample in self.samples.values():
 			if (sample.depth >= site_depth and
 					sample.depth/sample.mean_depth <= site_ratio):
@@ -97,43 +95,62 @@ class GenomicSite:
 			else:
 				sample.keep = False
 
-	def filter_site(self, site_prev=None, site_maf=None, site_type=None, site_freq=None):
+	def filter(self, site_prev=None, site_maf=None, site_type=None):
 		""" determine if site passes quality control """
 		if self.ref_allele not in ['A','T','C','G']:
 			self.keep = False
-		if site_prev and self.prev < max(1e-6, site_prev):
+		if site_prev and self.prevalence < max(1e-6, site_prev):
 			self.keep = False
 		elif site_maf and self.minor_freq < site_maf:
 			self.keep = False
-		elif site_type and self.info['site_type'] not in site_type:
+		elif site_type and self.site_type not in site_type:
 			self.keep = False
 		else:
 			self.keep = True
 
-	def prevalence(self):
+	def compute_prevalence(self):
 		self.count_samples = sum([1 for s in self.samples.values() if s.keep])
 		return float(self.count_samples)/len(self.samples)
 
-	def summary_stats(self, weighted=False):
+	def summary_stats(self):
 		""" compute summary stats for site across samples """
-		self.prev = self.prevalence()
-		self.mean_freq = self.compute_mean_freq(weighted)
-		self.minor_freq = min(self.mean_freq, 1-self.mean_freq)
+		self.prevalence = self.compute_prevalence()
+		self.pooled_maf = self.compute_pooled_maf()
 
-	def compute_mean_freq(self, weighted):
+	def compute_pooled_maf(self):
 		""" compute average frequency of reference allele with optional weighting of samples """
 		if self.count_samples == 0:
 			return 0.0
 		else:
-			return np.mean([s.ref_freq for s in self.samples.values() if s.keep])
+			return np.mean([s.freq for s in self.samples.values() if s.keep])
 
-def fetch_samples(species, mean_depth=0, fract_cov=0, max_samples=float('inf'), keep_samples=None, exclude_samples=None):
-	""" List and select samples from input """
+	def resample_reads(self, rand_reads, replace_reads):
+		""" resample random number of reads per sample """
+		index = 0
+		for sample in self.samples.values():
+			sample.depth = rand_reads
+			if sample.freq > 0 and sample.freq < 1:
+				count_minor = int(round(sample.freq * sample.depth))
+				count_major = sample.depth - count_minor
+				alleles = np.random.choice([1]*count_minor+[0]*count_major, rand_reads, replace=replace_reads)
+				sample.freq = np.mean(alleles)
+			index += 1
+
+def fetch_samples(species, mean_depth=0, fract_cov=0, max_samples=float('inf'),
+                  keep_samples=None, exclude_samples=None, rand_samples=None):
+	""" Select samples from input
+		mean_depth: filter samples based on average genome/gene depth
+		fract_cov: filter samples based on the number of genomic sites covered by >=1 read
+		max_samples: stop when this numbe of samples reached
+		keep_samples: only keep samples in this list
+		exclude_samples: exclude any sample in this list
+		rand_samples: select random subset of samples
+	"""
 	samples = {}
-	for info in species.files['summary']:
-		# init sample
+	# select samples that pass filters
+	for index, info in enumerate(species.files['summary']):
 		sample = Sample(info)
-		# excludes sample
+		sample.index = index
 		if sample.filter(mean_depth, fract_cov):
 			continue
 		if keep_samples and sample.id not in keep_samples:
@@ -142,10 +159,21 @@ def fetch_samples(species, mean_depth=0, fract_cov=0, max_samples=float('inf'), 
 			continue
 		if len(samples) >= max_samples:
 			continue
-		# store sample
 		samples[sample.id] = sample
+	# check there's at least 1 sample
 	if len(samples) == 0:
-		sys.exit("\nError: no samples satisfied your selection criteria.\nTry running again with more lenient parameters\n")
+		error = "\nError: no samples satisfied your selection criteria.\n"
+		error += "Try running again with more lenient parameters\n"
+		sys.exit(error)
+	# select random subset of samples
+	if rand_samples:
+		if rand_samples > len(samples):
+			error = "\nError: --rand_samples cannot exceed the number of samples\n"
+			sys.exit(error)
+		ids = np.random.choice(samples.keys(), rand_samples, replace=False)
+		for id in samples.keys():
+			if id not in ids:
+				del samples[id]
 	return samples
 
 
@@ -159,3 +187,4 @@ def fetch_sites(species, samples):
 		else:
 			index += 1
 			yield site
+
